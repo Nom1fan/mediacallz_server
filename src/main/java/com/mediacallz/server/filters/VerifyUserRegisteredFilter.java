df34993e93605eb775806ddad571a2d9cdf3f36c
Mor <mormerhav@gmail.com>
2016-09-18 12:46:11 +0300

    Trying to fix user verification

--- src/main/java/com/mediacallz/server/filters/VerifyUserRegisteredFilter.java
+++ src/main/java/com/mediacallz/server/filters/VerifyUserRegisteredFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -19,7 +20,7 @@ import java.util.logging.Logger;
 @Component
 public class VerifyUserRegisteredFilter implements Filter {
 
-    private List<PreRegistrationController> preRegistrationControllers;
+    private List<String> preRegistrationUrls;
 
     @Autowired
     Logger logger;
@@ -29,7 +30,10 @@ public class VerifyUserRegisteredFilter implements Filter {
 
     @Autowired
     public void initPreRegistrationList(List<PreRegistrationController> preRegistrationControllers) {
-        this.preRegistrationControllers = preRegistrationControllers;
+        preRegistrationUrls = new ArrayList<>(preRegistrationControllers.size());
+        for (PreRegistrationController preRegistrationController : preRegistrationControllers) {
+            preRegistrationUrls.add(preRegistrationController.getUrl());
+        }
     }
 
 
@@ -43,22 +47,39 @@ public class VerifyUserRegisteredFilter implements Filter {
         if (servletRequest instanceof HttpServletRequest) {
             boolean verificationOK = true;
             String url = ((HttpServletRequest) servletRequest).getRequestURL().toString();
-            for (PreRegistrationController preRegistrationController : preRegistrationControllers) {
-                if(!url.contains(preRegistrationController.getUrl())) { // The request requires the user to already be registered
-                    String messageInitiaterId = servletRequest.getParameter(DataKeys.MESSAGE_INITIATER_ID.toString());
+
+            String messageInitiaterId = servletRequest.getParameter(DataKeys.MESSAGE_INITIATER_ID.toString());
+            if (messageInitiaterId == null) {
+                verificationOK = false;
+                sendForbiddenError((HttpServletResponse) servletResponse, url, ((HttpServletRequest) servletRequest).getRemoteUser());
+            }
+            else if (!isPreRegistrationRequest(url)) { // The request requires the user to already be registered
                     boolean isRegistered = usersDataAccess.isRegistered(messageInitiaterId);
                     if (!isRegistered) {
                         verificationOK = false;
-                        logger.warning("User " + messageInitiaterId + " attempted a request in url:" + url + " but is unregistered. Request was blocked.");
-                        HttpServletResponse httpResponse = (HttpServletResponse)servletResponse;
-                        httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
-                        break;
+                        sendForbiddenError((HttpServletResponse) servletResponse, url, messageInitiaterId);
                     }
-                }
             }
-            if(verificationOK)
+
+            if (verificationOK)
                 filterChain.doFilter(servletRequest, servletResponse);
         }
+    }
+
+    private void sendForbiddenError(HttpServletResponse servletResponse, String url, String messageInitiaterId) throws IOException {
+        logger.warning("User " + messageInitiaterId + " attempted a request in url:" + url + " but is unregistered. Request was blocked.");
+        servletResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+
+    private boolean isPreRegistrationRequest(String url) {
+        boolean result = false;
+        for (String preRegistrationUrl : preRegistrationUrls) {
+            if(url.contains(preRegistrationUrl)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
