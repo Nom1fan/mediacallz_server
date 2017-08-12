cc1f8a2dc54d5acbda9b4912ca389affabdc6e55
Mor <mormerhav@gmail.com>
2017-08-12 18:45:17 +0300

    Fixing bug in SyncContacts in case list is empty

--- src/main/java/com/mediacallz/server/model/request/SyncContactsRequest.java
+++ src/main/java/com/mediacallz/server/model/request/SyncContactsRequest.java
@@ -5,6 +5,7 @@ import com.mediacallz.server.model.dto.ContactDTO;
 import com.mediacallz.server.validators.Uid;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -17,5 +18,6 @@ import java.util.List;
 public class SyncContactsRequest extends Request {
 
     @Valid
+    @NotEmpty
     private List<ContactDTO> contacts;
 }
