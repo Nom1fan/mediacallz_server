0116ed68721a0cb908c2183f704d9e58fe299b4a
Mor <mormerhav@gmail.com>
2018-03-28 00:57:25 +0300

    Fix GuidValidator tests + SpecialMediaTypeFilter for no_validations branch

--- src/test/java/com/mediacallz/server/GuidValidatorTests.java
+++ src/test/java/com/mediacallz/server/GuidValidatorTests.java
@@ -5,7 +5,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,8 +18,7 @@ import java.util.UUID;
 @SpringBootTest
 public class GuidValidatorTests {
 
-    @Autowired
-    GUIDValidator guidValidator;
+    private GUIDValidator guidValidator = new GUIDValidator();
 
     @Mock
     private ConstraintValidatorContext constraintValidatorContextMock;
